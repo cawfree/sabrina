2bb38b1806f595654ae9dfaa2a3de3a24fd55cc1
cawfree <hello@cawfree.com>
2020-02-18 00:09:10 +0000

    Added dark mode toggle.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import DarkModeToggle from "react-dark-mode-toggle";
 import useDarkMode from "use-dark-mode";
 
 import createStore from "./createStore";
@@ -12,11 +13,29 @@ const store = createStore();
 const { dispatch, getState } = store;
 
 const App = ({ ...extraProps }) => {
-  const { value, toggle } = useDarkMode(true);
+  const [isDarkMode, setIsDarkMode] = useState(true);
+  const { value, toggle } = useDarkMode(isDarkMode);
   useEffect(() => dispatch(openSocket()) && undefined, []);
   return (
     <Provider store={store}>
       <PaneContainer />
+      <div
+        style={{
+          zIndex: 10,
+          position: 'absolute',
+          bottom: 10,
+          right: 10,
+        }}
+      >
+        <DarkModeToggle
+          size={80}
+          onChange={() => [
+            setIsDarkMode(!isDarkMode),
+            setTimeout(toggle, 500),
+          ]}
+          checked={!isDarkMode}
+        />
+      </div>
     </Provider>
   );
 };
